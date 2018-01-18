4d3222beb10fa012f13ffa6ee9699737e8c38ced
jwon114 <jthgwong@gmail.com>
2018-01-18 21:57:46 +1100

    item styling, ratings, review input box

--- main.rb
+++ main.rb
@@ -75,7 +75,7 @@ post '/users/create' do
 		redirect '/users/new'
 	else
 		new_user = User.create(name: params[:name], email: params[:email], password: params[:password])
-		redirect '/store'
+		redirect '/users/login'
 	end
 end
 
